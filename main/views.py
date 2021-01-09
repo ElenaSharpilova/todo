407e7ad082fd2b4111bcf5aad5c309a7a178cb3c
ElenaSharpilova <esharpilova@gmail.com>
2021-01-09 10:00:56 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, HttpResponse
+
+def homepage(request):
+    return HttpResponse("Hello world!")
+
+def test(request):
+    return render(request, "test.html")
+
+def second(request):
+    return HttpResponse("test 2 page")
+
